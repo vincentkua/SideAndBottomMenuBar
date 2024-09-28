8c3a4d38b98c81b5385d395b14a9366fd695c9a4
vincentkua <zylyn89@gmail.com>
2024-09-28 23:04:04 +0800

    feat: ui design update to side menubar

--- src/components/MenubarSide.tsx
+++ src/components/MenubarSide.tsx
@@ -1,37 +1,44 @@
-import { HomeIcon, InfoIcon, SettingsIcon, ShoppingCartIcon } from "lucide-react";
-import { SideMenubarDivBox } from "./styled.component";
+import {
+  FlameIcon,
+  HomeIcon,
+  InfoIcon,
+  SettingsIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
+import {
+  SideMenuBarArea,
+  SideMenubarDivBox,
+  SideMenuLogo,
+} from "./styled.component";
 import { useNavigate } from "react-router-dom";
 
 const MenubarSide = () => {
-
   const navigate = useNavigate();
 
   return (
     <>
-      <div
-        style={{
-          border: "1px solid gray",
-          width: "200px",
-          backgroundColor: "#2e2e2e",
-        }}
-      >
-        <SideMenubarDivBox onClick={()=>navigate("/")}>
-          <HomeIcon color="lightgray" size={24} style={{ margin: "10px" }} />
-          <p style={{ color: "lightgray" }}>Home</p>
+      <SideMenuBarArea>
+        <SideMenuLogo>
+          <FlameIcon fill="red" strokeWidth={0} size={24} style={{ marginRight: "15px" }}/>
+          <p>Sample App</p>
+        </SideMenuLogo>
+        <SideMenubarDivBox onClick={() => navigate("/")}>
+          <HomeIcon size={24} style={{ marginRight: "15px" }} />
+          <p>Home</p>
         </SideMenubarDivBox>
-        <SideMenubarDivBox onClick={()=>navigate("/products")}>
-          <ShoppingCartIcon color="lightgray" size={24} style={{ margin: "10px" }} />
-          <p style={{ color: "lightgray" }}>Products</p>
+        <SideMenubarDivBox onClick={() => navigate("/products")}>
+          <ShoppingCartIcon size={24} style={{ marginRight: "15px" }} />
+          <p>Products</p>
         </SideMenubarDivBox>
-        <SideMenubarDivBox onClick={()=>navigate("/about")}>
-          <InfoIcon color="lightgray" size={24} style={{ margin: "10px" }} />
-          <p style={{ color: "lightgray" }}>About</p>
+        <SideMenubarDivBox onClick={() => navigate("/about")}>
+          <InfoIcon size={24} style={{ marginRight: "15px" }} />
+          <p>About</p>
         </SideMenubarDivBox>
-        <SideMenubarDivBox onClick={()=>navigate("/settings")}>
-          <SettingsIcon color="lightgray" size={24} style={{ margin: "10px" }} />
-          <p style={{ color: "lightgray" }}>Settings</p>
+        <SideMenubarDivBox onClick={() => navigate("/settings")}>
+          <SettingsIcon size={24} style={{ marginRight: "15px" }} />
+          <p>Settings</p>
         </SideMenubarDivBox>
-      </div>
+      </SideMenuBarArea>
     </>
   );
 };
--- src/components/styled.component.tsx
+++ src/components/styled.component.tsx
@@ -1,14 +1,30 @@
 import styled from "styled-components";
 
+export const SideMenuBarArea = styled.div`
+  border: 1px solid gray;
+  width: 200px;
+  background-color: #1b1b1b;
+`;
+
+export const SideMenuLogo = styled.div`
+  display: flex;
+  align-items: center;
+  height: 60px;
+  padding: 12px;
+  color: lightgray;
+  font-weight: bold;
+`;
+
 export const SideMenubarDivBox = styled.div`
   display: flex;
   align-items: center;
   height: 48px;
   cursor: pointer;
-  padding: 10px;
+  padding: 12px;
+  color: #9e9e9e;
 
   &:hover {
-    background-color: #474747;
+    background-color: #2b2b2b;
+    color: #e6e6e6;
   }
-
 `;
